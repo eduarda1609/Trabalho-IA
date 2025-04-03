9d5784f50570510379ad537dbcd40f4f0e10a198
Eduarda Franzoni dos Santos <eduardafranzoni7@gmail.com>
2025-04-03 17:01:08 -0300

    Sobe trabalho

---
+++ openia.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const prompt = require('prompt-sync')()
+
+const { OpenAI } = require('openai');
+
+const openai = new OpenAI({ apiKey: "coloque a sua chave"});
+
+async function getOpenAIResponse(prompt) {
+    try {
+        const response = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: [{ role: "user", content: prompt }],
+            temperature: 0.7
+        });
+        console.log(response.choices[0].message.content);
+    } catch (error) {
+        console.error('Erro ao chamar OpenAI API:', error.response?.data || error.message);
+    }
+}
+
+
+const resposta = prompt("Me faça uma pergunta:  ")
+
+getOpenAIResponse(resposta)
